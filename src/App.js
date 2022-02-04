2bc493255d5582332847e839d64d3cde559af460
Amal221200 <amalmurikkoli2000@gmail.com>
2022-02-04 11:05:24 +0530

    Using Local Storage

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
 // import React from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
+import Footer from './components/Footer';
 
 
 function App() {
   const [showAddTask, setshowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 3 2022',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 4 2022',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Shopping',
-      day: 'Feb 5 2022',
-      reminder: false
-    }
-  ])
+  
+  // Fetching tasks 
+  let initTask;
+  if(localStorage.getItem('tasks') === null){
+    initTask = [];
+  }else{
+    initTask = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  const [tasks, setTasks] = useState(initTask);
 
   // Add Task
-
-  const addTask = (task) =>{
+  const addTask = async (task) =>{
     const id = Math.floor(Math.random() * 10000) + 1;
     const newTask = { id , ...task};
     setTasks([...tasks, newTask]);
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+  
     setTasks(tasks.map((task) =>
-      task.id === id ? { ...task, reminder: !task.reminder } : task))
+      task.id === id ? { ...task, reminder: !task.reminder } : task));
   }
 
-  let deleteTask = (id) => {
-    // console.log('deleting', id);
+  let deleteTask =async (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  useEffect(()=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks])
+
   return (
-    <div className="container">
-      <Header displayForm={() => setshowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}  />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to show'}
-    </div>
+    <Router>
+      <div className="container">
+    
+          {/* <Route path="/" element={()} /> */}
+          <Header displayForm={() => setshowAddTask(!showAddTask)} showAdd={showAddTask} />
+          
+          <Route path='/' exact render={(props)=>(
+            <>
+              {showAddTask && <AddTask onAdd={addTask}  />}
+              {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to show'}
+            </>
+          )} />
+          <Route path='/about' exact component={About} />
+          <Footer/>
+      
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
 const Header = ({ title, displayForm, showAdd }) => {
-
+    const location = useLocation();
+    
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd?'red':'green'} text={showAdd?'Close':'Add'} onClick={displayForm} />
+            {location.pathname === '/' && <Button color={showAdd?'red':'green'} text={showAdd?'Close':'Add'} onClick={displayForm} />}
         </header>
     );
 };
